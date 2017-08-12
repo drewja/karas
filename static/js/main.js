95a2b0c41d55d106940b3203bc333c8ee443cdfc
Andrew Arendt <AndrewArendt@gmail.com>
2017-08-12 10:35:27 -0500

    bunch a stuff

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,40 @@
-$.backstretch(["./img/KarasDental_team.tall.jpg"]);
+var loadPage = {
+    'index' : function(){
+        $.backstretch(["./img/KarasDental_team.tall.jpg"]);
+    },
+    'contact' : function(){
+        $('.page').load('./contact.html');
+    },
+    'about' : function(){
 
+    },
+    'contact' : function (){
 
+    },
+    'services' : function(){
+
+    },
+    'team' : function(){
+        $('.page').load('drkaras.html');    
+    }
+}
+
+function unloadPage(){
+
+    if ($("body").data("backstretch")) $.backstretch('destroy');
+    $('.page').empty();
+}
+
+$('.navbar-nav>li>a').click(
+    function(ev){
+        ev.preventDefault();
+        t = $(ev.target);
+        if (t.parent().hasClass('active')) return;
+        $('.navbar-nav>.active').removeClass('active');
+        t.parent().addClass('active')
+        page = t.data('target');
+        unloadPage();
+        loadPage[page]();
+        console.log(ev);
+    }
+)
